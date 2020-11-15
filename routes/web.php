7b126d57784ba086cc42912768a9a8ef402d1c35
utopia <maotsemarx1993@gmail.com>
2020-11-15 22:58:09 +0630

    Frontend Course and Teacher create

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,7 @@ Route::resource('company', 'CompanyController');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
